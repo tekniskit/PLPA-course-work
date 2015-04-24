7a8934c232136dd8be16a837ca51180bca60322a
Martin Jensen <martin2610@gmail.com>
2015-04-24 11:25:23 +0200

    Initial commit. robot.rkt loading another scheme file and writing log.txt.

---
+++ IronFist/Scheme/robot.rkt
@@ -0,0 +1,10 @@
+﻿;(define ns (make-base-namespace))
+
+;(include "factory.rkt")
+;(include "turn.rkt")
+(include "Scheme/move.rkt")
+
+;; Write to a text file test
+(call-with-output-file "log.txt"
+  (lambda (output-port)
+    (display "hello, world" output-port))) ;; or (write "hello, world" output-port)
