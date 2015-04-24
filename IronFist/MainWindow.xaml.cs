a34508ef7ed44c80bf9c718a44fb9e22dbf335df
Kasper Nissen <kaspernissen@gmail.com>
2015-04-24 12:07:05 +0200

    FileWatcher added to C# project

--- IronFist/MainWindow.xaml.cs
+++ IronFist/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -25,14 +26,35 @@ namespace IronFist
         {
             InitializeComponent();
             "(include \"Scheme/robot.rkt\")".Eval();
-
+            CreateFileWatcher();
         }
 
         private void RunButton_Click(object sender, RoutedEventArgs e)
         {
             ResultLabel.Content = InputA.Text.Eval();
-            // Eksempeler (define x (+ x 5)) (+ x 0)
-            // 
+        }
+
+        public void CreateFileWatcher()
+        {
+            // Create a new FileSystemWatcher and set its properties.
+            FileSystemWatcher watcher = new FileSystemWatcher();
+            watcher.Path = System.AppDomain.CurrentDomain.BaseDirectory;
+            watcher.NotifyFilter = NotifyFilters.LastWrite;
+            watcher.Filter = "log.txt";
+
+            // Add event handlers.
+            watcher.Changed += new FileSystemEventHandler(OnChanged);
+            watcher.Created += new FileSystemEventHandler(OnChanged);
+
+            // Begin watching.
+            watcher.EnableRaisingEvents = true;
+        }
+
+        // Define the event handlers.
+        private void OnChanged(object source, FileSystemEventArgs e)
+        {
+            var text = System.IO.File.ReadAllText(e.FullPath);
+            Application.Current.Dispatcher.Invoke(new Action(() => { LogTextBlock.Text = text; }));
         }
     }
 }
