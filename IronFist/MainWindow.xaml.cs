09cead2b444b6c606a1c13a7ffb30ac79a0300e8
KasperSaaby <kdsaaby@gmail.com>
2015-05-04 22:35:58 +0200

    Maphandler drawing map

--- IronFist/MainWindow.xaml.cs
+++ IronFist/MainWindow.xaml.cs
@@ -29,7 +29,7 @@ namespace IronFist
         {
             InitializeComponent();
             "(include \"Scheme/main.scm\")".Eval();
-            mapHandler = new MapHandler();
+            mapHandler = new MapHandler(MapCanvas);
             mapHandler.DrawMap();
             
             CreateFileWatcher();
