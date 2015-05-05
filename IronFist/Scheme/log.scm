809c41a9dcffcb47397f784a7b94c84734de446c
Rasmus Reimer <rasmus.reimer@gmail.com>
2015-05-05 21:13:41 +0200

    The log now also writes to the terminal.

--- IronFist/Scheme/log.scm
+++ IronFist/Scheme/log.scm
@@ -1,4 +1,8 @@
+; Function: log
+; Description: Logs x, y, direction and cargo to log.txt in the format: {x};{y};{direction};{cargo}\n\r
+
 (define (log x y direction cargo) 
   (define test-file (open-output-file "log.txt"))
+  (display (string-append (number->string x) ";" (number->string y) ";" (number->string direction) ";" cargo "\n\r"))
   (display (string-append (number->string x) ";" (number->string y) ";" (number->string direction) ";" cargo "\n\r") test-file)
   (close-output-port test-file))
