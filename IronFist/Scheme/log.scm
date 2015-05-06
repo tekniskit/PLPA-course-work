527e00808755d92a1b976e5cb302232c1eda7554
Rasmus Reimer <rasmus.reimer@gmail.com>
2015-05-06 13:51:53 +0200

    removed debug code.

--- IronFist/Scheme/log.scm
+++ IronFist/Scheme/log.scm
@@ -2,7 +2,6 @@
 ; Description: Logs x, y, direction and cargo to log.txt in the format: {x};{y};{direction};{cargo}\n\r
 
 (define (log x y direction cargo) 
-  (define test-file (open-output-file "log.txt"))
-  (display (string-append (number->string x) ";" (number->string y) ";" (number->string direction) ";" cargo "\n\r"))
-  (display (string-append (number->string x) ";" (number->string y) ";" (number->string direction) ";" cargo "\n\r") test-file)
-  (close-output-port test-file))
+  (define file-handle (open-output-file "log.txt"))
+  (display (string-append (number->string x) ";" (number->string y) ";" (number->string direction) ";" cargo "\n\r") file-handle)
+  (close-output-port file-handle))
