a56fe1e1ada59a3a80b964878e1746a983a9dbda
Rasmus Reimer <rasmus.reimer@gmail.com>
2015-05-18 14:05:00 +0200

    Added turn unit tests.

--- IronFist/Scheme/Tests/turn.scm
+++ IronFist/Scheme/Tests/turn.scm
@@ -1,7 +1,39 @@
+(it-should "turn -90 deg left" (lambda ()
+	(turn_left -1)
+	(= direction 3)))
+
+(it-should "turn 0 deg left" (lambda ()
+	(turn_left 0)
+	(= direction 0)))
+
 (it-should "turn 90 deg left" (lambda ()
 	(turn_left 1)
 	(= direction 1)))
 
+(it-should "turn 180 deg left" (lambda ()
+	(turn_left 2)
+	(= direction 2)))
+
+(it-should "turn 900 deg left" (lambda ()
+	(turn_left 10)
+	(= direction 2)))
+
+(it-should "turn -90 deg right" (lambda ()
+	(turn_right -1)
+	(= direction 1)))
+
+(it-should "turn 0 deg right" (lambda ()
+	(turn_right 0)
+	(= direction 0)))
+
 (it-should "turn 90 deg right" (lambda ()
 	(turn_right 1)
 	(= direction 3)))
+
+(it-should "turn 180 deg right" (lambda ()
+	(turn_right 2)
+	(= direction 2)))
+
+(it-should "turn 900 deg right" (lambda ()
+	(turn_right 10)
+	(= direction 2)))
