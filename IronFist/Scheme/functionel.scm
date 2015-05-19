88501833da909f428495a2d3261de45e122b08a5
Rasmus Reimer <rasmus.reimer@gmail.com>
2015-05-19 19:39:09 +0200

    bug fix.

--- IronFist/Scheme/functionel.scm
+++ IronFist/Scheme/functionel.scm
@@ -50,13 +50,14 @@
          (next-x (if (even? dir) (+ x (* forward? (if (= 0 dir) 1 -1))) x))
          (next-y (if (odd? dir) (+ y (* forward? (if (= 3 dir) 1 -1))) y)))
     
-    (cond ((> distance 0)  
+    (if (> distance 0)  
            (if (and (>= next-y 0) (< next-y (vector-length factory))
                     (>= next-x 0) (< next-x (vector-length (vector-ref factory next-y)))
                     (member (get-tile next-x next-y) '(* ! ^ v < >)))
                
                (move (set-pos robot next-x next-y) (- distance 1) forward?)
-               (log-error "Illegal move. Robot stopped."))))))
+               (log-error "Illegal move. Robot stopped."))
+           robot)))
 
 (define (move_forward robot distance)
   (move robot distance 1))
