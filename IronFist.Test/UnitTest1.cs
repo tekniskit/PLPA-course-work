945b7a67c2af3ccb21027246f654bcfb6105dd75
Kasper Nissen <kaspernissen@gmail.com>
2015-05-20 11:59:14 +0200

    TestProject added

---
+++ IronFist.Test/UnitTest1.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using IronScheme;
+
+namespace IronFist.Test
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestInitialize]
+        public void Setup()
+        {
+            "(include \"Scheme/main.scm\")".Eval();
+        }
+
+        [TestMethod]
+        public void RunTest()
+        {
+            Assert.AreEqual("(include \"Scheme/Tests/test.scm\")".Eval(),"Lala");
+        }
+    }
+}
